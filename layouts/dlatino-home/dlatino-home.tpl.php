b9f8a9b38c02e6a1caefff9f7ddc4894edffcd1d
Joaquin Bravo Contreras <jackbravo@gmail.com>
2011-11-28 07:26:08 -0800

    Make dlatino-home panel layout work also for gdl2012 theme

--- layouts/dlatino-home/dlatino-home.tpl.php
+++ layouts/dlatino-home/dlatino-home.tpl.php
@@ -1,14 +1,14 @@
 <div class="panel-display panel-gdl2012-home clear-block" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel panel-sidebar-left alpha grid16-10">
+  <div class="panel-panel panel-sidebar-left alpha grid16-10 grid_8">
     <div class="inside"><?php print $content['left']; ?><div class="clear">&nbsp;</div></div>
   </div>
 
-  <div class="panel-panel panel-sidebar-right omega grid16-6">
+  <div class="panel-panel panel-sidebar-right omega grid16-6 grid_4">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
   <div class="clear">&nbsp;</div>
 
-  <div class="panel-sponsors grid16-16">
+  <div class="panel-sponsors grid16-16 grid_12">
     <div class="inside"><?php print $content['sponsors'];?></div>
   </div>
 </div>
